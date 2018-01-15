97590d34b95d67f80c0a3b6c6479597cf714d710
user <zzang2314274@hanmail.net>
2018-01-15 17:17:31 +0900

    URL 수리

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,7 +21,7 @@ from mysite.views import HomeView, UserRegisterView, UserRegisterDoneView, valid
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/$', include('django.contrib.auth.urls')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^accounts/register/$', UserRegisterView.as_view(), name='register'),
     url(r'^accounts/register/done/$', UserRegisterDoneView.as_view(), name='register_done'),
     url(r'^ajax/validate_username/$', validate_username, name="validate_username"),
